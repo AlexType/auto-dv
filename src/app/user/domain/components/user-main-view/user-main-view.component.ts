8cecea89e92b6969a0a96d85a11eb5cbc2b16ad3
Alexandr Bryzgalov <914033@mail.ru>
2023-03-05 19:28:31 +0300

    добавил адаптивную вёрстку

--- src/app/user/domain/components/user-main-view/user-main-view.component.ts
+++ src/app/user/domain/components/user-main-view/user-main-view.component.ts
@@ -1,10 +1,10 @@
 import { ReviewDto } from './../../../../api/dto/review.dto';
 import { Output, Input } from '@angular/core';
 import { Component, EventEmitter } from '@angular/core';
-import SwiperCore, { Grid, SwiperOptions } from 'swiper';
+import SwiperCore, { Grid, SwiperOptions, Autoplay } from 'swiper';
 import { Car } from 'src/app/api/dto/car.dto';
 
-SwiperCore.use([Grid]);
+SwiperCore.use([Grid, Autoplay]);
 
 @Component({
   selector: 'app-user-main-view',
@@ -19,23 +19,63 @@ export class UserMainViewComponent {
   @Output() sendRequest: EventEmitter<void> = new EventEmitter();
 
   protected swiperConfig: SwiperOptions = {
-    // modules: [Grid],
-    slidesPerView: 4,
-    spaceBetween: 25,
-    slidesPerGroup: 3,
     navigation: false,
-    grid: {
-      rows: 2,
-      fill: "row"
-    },
+    autoplay: true,
     scrollbar: { draggable: true },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 25,
+        slidesPerGroup: 1,
+        grid: {
+          rows: 2,
+          fill: "row"
+        },
+      },
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 25,
+        slidesPerGroup: 1,
+        grid: {
+          rows: 2,
+          fill: "row"
+        },
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 2,
+          fill: "row"
+        },
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 25,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 2,
+          fill: "row"
+        },
+      }
+    }
   }
 
   protected swiperConfig2: SwiperOptions = {
     slidesPerView: 2,
     spaceBetween: 50,
     navigation: false,
+    autoplay: true,
     scrollbar: { draggable: true },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      992: {
+        slidesPerView: 2,
+      }
+    }
   }
 
   ngOnInit() { }
